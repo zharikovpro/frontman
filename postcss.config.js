ef0d173ade8e8555a8e18140afbcce32c8a5cad6
Govorov Nikolai <nikolatha@mail.ru>
2016-11-05 23:04:09 +0600

    Writed metalsmith-webpack-plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   plugins: {
+    stylelint: {},
     autoprefixer: {},
     lost: {},
+    'postcss-reporter': {
+      clearMessages: true,  
+    },
   },
+  path: '../css/',
   map: true,
 };
 
